34b68b13259ac34a557ce1f43cc82889ccca8b40
IrregularPersona <148436276+IrregularPersona@users.noreply.github.com>
2024-02-08 23:47:03 +0700

    Updated main.c
    Added more things (such as some proper constraints to certain inputs) to fit the requirements of the project. 
    
    
    DISCLAIMER: Not done yet! :D

--- main.c
+++ main.c
@@ -7,6 +7,19 @@
 
 #define MAX_WORD_LENGTH  100
 
+void enterToContinue() {
+	printf("Press enter to continue.\n");
+	while(getchar() != '\n');
+}
+
+void loadingScreen() {
+	printf("Loading");
+	for(int i = 0; i < 3; i++) {
+		printf(".");
+		Sleep(200);
+	}
+}
+
 int main() {
 	trienode *root = NULL;
 	char word[MAX_WORD_LENGTH];
@@ -14,57 +27,63 @@ int main() {
 	int choice;
 
 	while (true) {
+		system("cls");
 		printf("\nMenu:\n");
-		printf("1. Insert word with description\n");
-		printf("2. Search for word\n");
-		printf("3. Delete word\n");
-		printf("4. Exit\n");
+		printf("1. Release a new slang word\n");
+		printf("2. Search a slang word\n");
+		printf("3. View all slang words starting with a certain prefix word\n");
+		printf("4. View all slang words\n");
+		printf("5. Exit\n");
 		printf("Enter your choice: ");
 		scanf("%d", &choice);
 
 		switch (choice) {
 			case  1:
-				printf("Enter word: ");
-				scanf("%s", word);
+				printf("Input a new slang word [Must be more than 1 characters and contains no space]: ");
+				scanf("%99s", word);
 				getchar();
-				printf("Enter description: ");
-				scanf("%s", description);
+				while(strlen(word) < 2 || strchr(word, ' ') != NULL) {
+					printf("Please enter the word with the expected requirements!\n");
+					printf(">> ");
+					scanf("%99s", word);
+				}
+				printf("Input a new slang word description [Must be more than 2 words]: ");
+				scanf("%99s", description);
 				getchar();
 				trieInsert(&root, word);
-				printf("Word '%s' inserted.\n", word);
+				printf("Slang '%s' inserted.\n", word);
 				Sleep(2500);
-				system("cls");
 				break;
 			case  2:
-				printf("Enter word to search for: ");
-				scanf("%s", word);
+				printf("Enter slang to search for [Must be more than 1 characters and contains no space]: ");
+				scanf("%99s", word);
 				getchar();
+				while(strlen(word) < 2 || strchr(word, ' ') != NULL) {
+					printf("Please enter the word with the expected requirements!\n");
+					printf(">> ");
+					scanf("%99s", word);
+				}
 				if (search_trie(root, word)) {
-					printf("Word '%s' found.\n", word);
-					printf("Description: %s", description);
+					printf("Slang word : %99s\n", word);
+					printf("Description : %99s", description);
 					Sleep(2500);
-					system("cls");
 				} else {
 					printf("Word not found.\n");
 					Sleep(2500);
-					system("cls");
 				}
 				break;
-			case  3:
-				printf("Enter word to delete: ");
+			case 3:
+				printf("Input a prefix to be searched: ");
 				scanf("%s", word);
-				getchar();
-				if (deleteContents(root, word)) {
-					printf("Word '%s' deleted.\n", word);
-					Sleep(2500);
-					system("cls");
-				} else {
-					printf("Word not found.\n");
-					Sleep(2500);
-					system("cls");
-				}
+				search_trie(root, word);
 				break;
 			case  4:
+				printtrie(root);
+				Sleep(3000);
+
+				enterToContinue();
+				break;
+			case  5:
 				printf("Exiting program.\n");
 				exit(EXIT_SUCCESS);
 			default:
